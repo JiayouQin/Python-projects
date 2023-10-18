ccd34fa9bd56bcf46ae4da1b7e57c197068f25a6
Jiayou Qin <46877249+JiayouQin@users.noreply.github.com>
2023-10-18 13:38:09 +0800

    Update fixedbase.py

--- 21 Inverse Kinematics Demo/fixedbase.py
+++ 21 Inverse Kinematics Demo/fixedbase.py
@@ -13,18 +13,24 @@ class Joints():
     x_, y_ = 0,0
     color = (255,255,255)
     parent = None
+    child = None
+    index = 0
     total_joints = []
-    def __init__(self, x, y, angle, length, parent = None):
-        Joints.total_joints.append(self)
+
+    def __init__(self, x, y, angle, length):
+        
         self.x = x #initial point
         self.y = y
         self.x_ = 0 #endpoint
         self.y_ = 0
         self.angle = angle
         self.length = length    #given length of the arm
-        self.base = (100,100)
-        if parent:  #parent's end point is the base point of the joint
-            self.parent = parent
+        self.base = (int(screen_size[0]/2),int(screen_size[1]/2))   #base of arm
+        self.index = len(Joints.total_joints)
+        Joints.total_joints.append(self)
+        if self.index != 0:  #parent's end point is the base point of the joint
+            self.parent = Joints.total_joints[self.index-1]
+            self.parent.child = self    #provide link from both ends, double linked list
 
     def inverse(self):
         dx = self.x_ - self.x 
@@ -37,23 +43,28 @@ class Joints():
             self.parent.y_ = self.y
             self.parent.inverse()
         
-    def calc(self): #calculation of inverse kinematics
-        self.inverse()
 
+    def foward(self):
+        if self.parent:
+            self.x,self.y = self.parent.x_, self.parent.y_
+        else:
+            self.x,self.y = self.base
+        self.x_ = self.length * math.cos(self.angle) + self.x  
+        self.y_ = self.length * math.sin(self.angle) + self.y  #project the length back and reset base
+        self.draw()
+        if self.child:
+            self.child.foward()
+                
     def draw(self):
         pg.draw.line(screen,self.color,(self.x,self.y),(self.x_,self.y_),3)
 
     @staticmethod
-    def foward():
-        for joint in Joints.total_joints:
-            if joint.parent:
-                joint.x,joint.y = joint.parent.x_, joint.parent.y_
-            else:
-                joint.x,joint.y = (100,100)
-            joint.x_ = joint.length * math.cos(joint.angle) + joint.x  
-            joint.y_ = joint.length * math.sin(joint.angle) + joint.y  #project the length back and reset base
-            
-            joint.draw()
+    def calc(x,y):
+        end_joint = Joints.total_joints[-1]
+        end_joint.x_, end_joint.y_ = x, y
+        end_joint.inverse()
+        Joints.total_joints[0].foward()
+
 
 screen_size = [800,800]
 grid_size = [40,40]
@@ -66,20 +77,16 @@ pg.init()
 path = [[0,0]]
 
 pg.display.update()
-joint1 = Joints(100,100,0,100)
-joint2 = Joints(100,100,0,150,joint1)
-joint3 = Joints(100,100,0,50,joint2)
-joint4 = Joints(100,100,0,80,joint3)
+
+Joints(100,100,0,100)
+Joints(100,100,0,150)
+Joints(100,100,0,50)
+Joints(100,100,0,80)
 
 while run:
     x,y = pg.mouse.get_pos()
     screen.fill((0,0,0))
-
-    joint4.x_,joint4.y_ = x, y
-    joint4.calc()
-    Joints.foward()
-
-
+    Joints.calc(x,y)
     pg.display.update()
 
     for event in pg.event.get():
